16f199371901c483df23697aee17555e11670030
DogSniffers <DogSniffers@users.noreply.github.com>
2020-04-19 02:31:05 -0600

    still testing what i want to do

--- src/Components/Info/Info.js
+++ src/Components/Info/Info.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import './Info.css'
 
 function Info(){
     return(
         <div>
             <h1>Info</h1>
+            <h2>Fact</h2>
+            <h2>Technologies</h2>
+            <h2>Contact Me</h2>
         </div>
     )
 }
--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,35 +1,22 @@
-import React, { useState } from 'react'
-import Button from '@material-ui/core/Button';
-import Menu from '@material-ui/core/Menu'
-import { MenuItem } from '@material-ui/core';
+import React, { useState } from 'react';
+import Button from '@material-ui/core/Button'
+import './Nav.css'
 
+ function Nav() {
 
-function Nav(props){
-    const [anchorEl, setAnchorEl] = useState(null);
+     const handleNavigate = (test) => {
+         const boof = document.getElementById(test)
+         boof.scrollIntoView({})
 
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-    
-
-    return(
-        <>
-        <Button aria-controls="menu" aria-haspopup="true" onClick={handleClick} >Navigation</Button>
-        <Menu 
-            id="menu"
-            anchorEl={anchorEl}
-            keepMounted
-            open={Boolean(anchorEl)}
-            onClose={handleClose}>
-                <MenuItem onClick={handleClose}>Test</MenuItem>
-                <MenuItem onClick={handleClose}>Test 2</MenuItem>
-        </Menu>
-        </>
-    )
+     }
+  return (
+    <div className='navbar'>
+        <h1>Navigation</h1>
+        <Button>Test 1</Button>
+        <Button>Test 2</Button>
+      
+      
+    </div>
+  );
 }
 export default Nav
-
