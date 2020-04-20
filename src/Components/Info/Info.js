627e032f203900852ebf0722c496b8dc9378e7c0
DogSniffers <DogSniffers@users.noreply.github.com>
2020-04-19 21:45:59 -0600

    basic skills inputted

--- src/Components/Info/Info.js
+++ src/Components/Info/Info.js
@@ -10,14 +10,44 @@ function Info(){
             </div>
             <div>
                 <h1 className='subtitle'>Skills</h1>
+                <div>
+                    <h2>Languages</h2>
+                    <p>Javascript</p>
+                </div>
+                <div>
+                    <h2>Front-End</h2>
+                    <p>React</p>
+                    <p>HTML</p>
+                    <p>CSS</p>
+                </div>
+                <div>
+                    <h2>Back-End</h2>
+                    <p>Node.js</p>
+                    <p>SQL Tabs</p>
+                    <p>React Redux</p>
+                </div>
+                <div>
+                    <h2>Additional Technologies</h2>
+                    <p>Axios</p>
+                    <p>Sass</p>
+                    <p>Chart.js</p>
+                    <p>Socket.io</p>
+                    <p>Material Ui</p>
+                </div>
             </div>
             <div>
                 <h1 className='subtitle'>Projects</h1>
                 <div>
                     <h2>Personal Project</h2>
+                    <h3>The Gauntlet</h3>
+                    <h4>Info</h4>
+                    <p>Project Info</p>
                 </div>
                 <div>
                     <h2>Group Project</h2>
+                    <h3>Boardgame App</h3>
+                    <h4>Info</h4>
+                    <p>Project Info</p>
                 </div>
             </div>
             <div>
