98e828a88bb8617c0cb33d49f53b075ae8e76ffd
DogSniffers <DogSniffers@users.noreply.github.com>
2020-04-20 14:26:01 -0600

    info section content added

--- src/Components/Info/Info.js
+++ src/Components/Info/Info.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import Button from '@material-ui/core/Button'
-import Box from '@material-ui/core/Box'
+import { makeStyles } from '@material-ui/core/styles'
 import './Info.css'
 
+const styles = makeStyles({
+    button:{
+        color:'#212121',
+        backgroundColor:'#424242',
+        border:'2',
+        width:'14vh',
+        '&:hover':{
+            backgroundColor:'#616161'
+        }
+    }
+})
+
 function Info(){
+    const style = styles();
     return(
         <div>
             <div>
@@ -37,29 +50,45 @@ function Info(){
                     <p>Material Ui</p>
                 </div>
             </div>
-            <div>
-                <Box>
+            <div className='project'>
                 <h1 className='subtitle'>Projects</h1>
                 <div>
                     <h2>Personal Project</h2>
                     <h3>The Gauntlet</h3>
                     <h4>Info</h4>
-                    <p className='projectInfo'>Project Info</p>
+                    <div className='projectInfo'>
+                        <p>Project Info:</p>
+                        <p>This was my Solo project through DevMountain. The app  utilized React, React Redux, Node.js, SQL tabs, and React-Router-Dom. Additionally I learned two additional technologies for this project, Sass and Chart.js.
+                        </p>
+                        <p>The app itself is a game that can be infinitely generated, and allows the user to create and add their own creatures into the game. The game plays similarly to Pokemon, by which their are turns for the player, and then the creature and all attacks hold 'types', and every creature/class hold their own weaknesses and resistances to these types. I also was able to implement color theme changes into the app through the use of Redux and conditional classes.
+                        </p>
+                        <Button>Github Link</Button>
+
+                    </div>
                 </div>
-                </Box>
-                <div className='project'>
+                <div>
                     <h2>Group Project</h2>
                     <h3>Boardgame App</h3>
                     <h4>Info</h4>
-                    <p className='projectInfo'>Project Info</p>
+                    <div className='projectInfo'>
+                        <p>Project Info:</p>
+                        <p></p>
+                        <p>This was my Group based project for DevMountain. The app
+                            utilized React, React Redux, Node.js, SQL tabs, React-Router-Dom, and Material UI. Additionally for this project, I was able to learn and implement Socket.io.
+                        </p>
+                        <p>This project was especially interesting, because it was our first introduction to the group coding environment, and the project started after the Covid-19 outbreak, which required us to be able to communicate from the at-home environment for this project. This proved to be challenging at times, but was an excellent learning experience.
+                        </p>
+                        <p>The app consists as a holder for online Boardgames. The initial plan for the app was to create chess, then add more games as we progressed. It proved difficult to implement chess, as there is a lot of behind the scenes logic that needs to be in place for each piece, so we were only able to get two games out before the due-date: Chess and Tic-Tac-Toe. My role in this project was to learn how to implement Socket.io into the app so that the user will be able to play with others online once the site is hosted. </p>
+                    </div>
+                    <Button>Github Link</Button>
                 </div>
             </div>
             <div>
                 <h1 className='subtitle'>Contact</h1>
                 <div className='linkButtons'>
-                    <Button variant='outlined' color='secondary' size='large' target='newTab' href='https://github.com/DogSniffers'>GitHub</Button>
-                    <Button variant='outlined' color='red' size='large' target='newTab' href='https://www.linkedin.com/in/noah-van-buskirk-221762161/'>LinkedIn</Button>
-                    <Button variant='outlined' size='large' >Email</Button>
+                    <Button variant='outlined' size='large' target='newTab' href='https://github.com/DogSniffers' className={style.button}>GitHub</Button>
+                    <Button variant='outlined' size='large' target='newTab' href='https://www.linkedin.com/in/noah-van-buskirk-221762161/' className={style.button}>LinkedIn</Button>
+                    <Button variant='outlined' size='large' className={style.button}>Email</Button>
                 </div>
             </div>
         </div>
