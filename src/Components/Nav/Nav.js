df0f7613b749155e4f5c8c53c8a480bc358f4b1c
DogSniffers <DogSniffers@users.noreply.github.com>
2020-04-19 20:44:45 -0600

    messing with material ui styling

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,8 +1,21 @@
 import React, { useState } from 'react';
 import Button from '@material-ui/core/Button'
+import { makeStyles } from '@material-ui/core/styles'
 import './Nav.css'
 
+const buttonStyles = makeStyles({
+    root:{
+        color:'#212121',
+        backgroundColor:'#424242',
+        border:'2',
+        '&:hover':{
+            backgroundColor:'#616161'
+        }
+    }
+})
+
  function Nav() {
+     const button = buttonStyles()
 
      const handleNavigate = (test) => {
          const boof = document.getElementById(test)
@@ -12,9 +25,9 @@ import './Nav.css'
   return (
     <div className='navbar'>
         <h1>Navigation</h1>
-        <Button>Skills</Button>
-        <Button>Projects</Button>
-        <Button>Contact</Button>
+        <Button className={button.root}>Skills</Button>
+        <Button className={button.root}>Projects</Button>
+        <Button className={button.root}>Contact</Button>
       
       
     </div>
